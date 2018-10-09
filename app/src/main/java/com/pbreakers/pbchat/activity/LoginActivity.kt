6068e247e6f4ee3e5f4c732261be607b1cf626a4
Eric Ampire <ericampire.top@gmail.com>
2018-10-09 23:15:17 +0200

    Tentative de connexion

--- app/src/main/java/com/pbreakers/pbchat/activity/LoginActivity.kt
+++ app/src/main/java/com/pbreakers/pbchat/activity/LoginActivity.kt
@@ -2,12 +2,28 @@ package com.pbreakers.pbchat.activity
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import com.pbreakers.pbchat.R
+import com.quickblox.core.QBEntityCallback
+import com.quickblox.core.exception.QBResponseException
+import com.quickblox.users.QBUsers
+import com.quickblox.users.model.QBUser
 
 class LoginActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        val user = QBUser("ericampire", "ericampire")
+        QBUsers.signIn(user, object : QBEntityCallback<QBUser> {
+            override fun onSuccess(user: QBUser, bundle: Bundle) {
+                Toast.makeText(baseContext, "Sign in", Toast.LENGTH_LONG).show()
+            }
+
+            override fun onError(error: QBResponseException) {
+                Toast.makeText(baseContext, error.message, Toast.LENGTH_LONG).show()
+            }
+        })
     }
 }
