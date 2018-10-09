0e10ffc04c7b89738c2d6246b58b36242d558a1c
Eric Ampire <ericampire.top@gmail.com>
2018-10-09 22:04:57 +0200

    Ajout de l'id menant vers l'appli facebook

--- app/src/main/java/com/pbreakers/pbchat/util/Config.kt
+++ app/src/main/java/com/pbreakers/pbchat/util/Config.kt
@@ -4,4 +4,5 @@ object QBChatConfig {
     val applicationId = "74066"
     val authorizationKey = "buGva2DAcGLDmjK"
     val authorizationSecret = "3GUMWaZmK3qKp7L"
+    val facebookAppId = "2113616232188635"
 }
