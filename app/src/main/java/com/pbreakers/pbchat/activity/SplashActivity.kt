9c89afc27bcba954117d3c21ef81533dcc7960a4
Eric Ampire <ericampire.top@gmail.com>
2018-10-09 22:23:39 +0200

    Je viens d'ajouter une autre information de configuration

--- app/src/main/java/com/pbreakers/pbchat/activity/SplashActivity.kt
+++ app/src/main/java/com/pbreakers/pbchat/activity/SplashActivity.kt
@@ -2,12 +2,34 @@ package com.pbreakers.pbchat.activity
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import com.pbreakers.pbchat.R
+import com.quickblox.auth.QBAuth
+import com.quickblox.auth.model.QBSession
+import com.quickblox.chat.QBChatService
+import com.quickblox.core.QBEntityCallback
+import com.quickblox.core.exception.QBResponseException
+import com.quickblox.users.model.QBUser
 
-class SplashActivity : AppCompatActivity() {
+class SplashActivity : AppCompatActivity(), QBEntityCallback<QBSession> {
+    override fun onSuccess(p0: QBSession?, p1: Bundle?) {
+        Toast.makeText(baseContext, "Okay", Toast.LENGTH_LONG).show()
+    }
+
+    override fun onError(p0: QBResponseException?) {
+        Toast.makeText(baseContext, "Error", Toast.LENGTH_LONG).show()
+
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
+
+        val qbChatService = QBChatService.getInstance()
+        qbChatService.
+
+        val user = QBUser("ericampire", "ericampire")
+
+        QBAuth.createSession(user, this)
     }
 }
--- app/src/main/java/com/pbreakers/pbchat/util/Config.kt
+++ app/src/main/java/com/pbreakers/pbchat/util/Config.kt
@@ -4,5 +4,7 @@ object QBChatConfig {
     val applicationId = "74066"
     val authorizationKey = "buGva2DAcGLDmjK"
     val authorizationSecret = "3GUMWaZmK3qKp7L"
+    val accoundKey = "74066"
+
     val facebookAppId = "2113616232188635"
 }
