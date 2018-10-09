41024192b83d52e5e428738a0a78f90ab33ec3b2
Eric Ampire <ericampire.top@gmail.com>
2018-10-09 21:43:51 +0200

    Mise en place des informations de configurations

--- app/src/main/java/com/pbreakers/pbchat/util/Config.kt
+++ app/src/main/java/com/pbreakers/pbchat/util/Config.kt
@@ -1,2 +1,7 @@
 package com.pbreakers.pbchat.util
 
+object QBChatConfig {
+    val applicationId = "74066"
+    val authorizationKey = "buGva2DAcGLDmjK"
+    val authorizationSecret = "3GUMWaZmK3qKp7L"
+}
