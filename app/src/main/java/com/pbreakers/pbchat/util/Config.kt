cab48707acbe156d040159f37ece3784b2195a98
Eric Ampire <ericampire.top@gmail.com>
2018-10-09 22:45:12 +0200

    Il es preferable que ça sois des constantes

--- app/src/main/java/com/pbreakers/pbchat/util/Config.kt
+++ app/src/main/java/com/pbreakers/pbchat/util/Config.kt
@@ -1,10 +1,10 @@
 package com.pbreakers.pbchat.util
 
 object QBChatConfig {
-    val applicationId = "74066"
-    val authorizationKey = "buGva2DAcGLDmjK"
-    val authorizationSecret = "3GUMWaZmK3qKp7L"
-    val accoundKey = "74066"
+    const val applicationId = "74066"
+    const val authorizationKey = "buGva2DAcGLDmjK"
+    const val authorizationSecret = "3GUMWaZmK3qKp7L"
+    const val accoundKey = "101368"
 
     val facebookAppId = "2113616232188635"
 }
