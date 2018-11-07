6862fa81f281c80e3367e1f3d8c757b5f5f63dab
Eric Ampire <ericampire.top@gmail.com>
2018-11-07 22:03:30 +0200

    Update dependance et ajout du item_contact.xml

--- app/src/main/java/com/pbreakers/pbchat/activity/login/LoginActivity.kt
+++ app/src/main/java/com/pbreakers/pbchat/activity/login/LoginActivity.kt
@@ -2,6 +2,7 @@ package com.pbreakers.pbchat.activity.login
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.pbreakers.pbchat.R
 
 class LoginActivity : AppCompatActivity() {
@@ -9,5 +10,6 @@ class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
     }
 }
