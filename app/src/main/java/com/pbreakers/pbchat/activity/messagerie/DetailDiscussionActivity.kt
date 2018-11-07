ee3db1c4c98c3d9027f50e00166e22a3ca368c2a
Eric Ampire <ericampire.top@gmail.com>
2018-11-08 00:29:51 +0200

    Envoye du message

--- app/src/main/java/com/pbreakers/pbchat/activity/messagerie/DetailDiscussionActivity.kt
+++ app/src/main/java/com/pbreakers/pbchat/activity/messagerie/DetailDiscussionActivity.kt
@@ -3,6 +3,11 @@ package com.pbreakers.pbchat.activity.messagerie
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.pbreakers.pbchat.R
+import com.sendbird.android.BaseChannel
+import com.sendbird.android.GroupChannel
+import com.sendbird.android.GroupChannelParams
+import com.sendbird.android.SendBird
+import kotlinx.android.synthetic.main.activity_detail_discussion.*
 
 class DetailDiscussionActivity : AppCompatActivity() {
 
@@ -10,13 +15,48 @@ class DetailDiscussionActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_detail_discussion)
 
+        var userId: String
+        var userProfile: String
+
         with(intent) {
 
             val userName = getStringExtra("userName")
-            val userProfile = getStringExtra("userProfile")
-            val userId = getStringExtra("userId")
+            userProfile = getStringExtra("userProfile")
+            userId = getStringExtra("userId")
 
             supportActionBar?.subtitle = userName
+        }
+
+
+        sendButton.setOnClickListener {
+            val messageText = tapedMessage.text.toString()
+
+            val ids = mutableListOf<String>(userId, SendBird.getCurrentUser().userId)
+            tapedMessage.text.clear()
+
+
+            val params = GroupChannelParams()
+                .setName(userProfile)
+                .setDistinct(true)
+                .setPublic(false)
+                .addUserIds(ids)
+
+            GroupChannel.createChannel(params) { e, a ->
+                if (a != null) {
+                    toast(a.message)
+                    return@createChannel
+                }
+
+                e.sendUserMessage(messageText) { a, error ->
+                    if (error != null) {
+                        toast(error.message)
+                        return@sendUserMessage
+                    }
+
+
+                    toast("message send")
+                }
+            }
         }
     }
 }
