a22eae8476fc3af4f8eeb5ccc13690495f26a985
Eric Ampire <ericampire.top@gmail.com>
2018-11-07 23:34:08 +0200

    Recuperation des informations

--- app/src/main/java/com/pbreakers/pbchat/activity/messagerie/DetailDiscussionActivity.kt
+++ app/src/main/java/com/pbreakers/pbchat/activity/messagerie/DetailDiscussionActivity.kt
@@ -9,5 +9,14 @@ class DetailDiscussionActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_detail_discussion)
+
+        with(intent) {
+
+            val userName = getStringExtra("userName")
+            val userProfile = getStringExtra("userProfile")
+            val userId = getStringExtra("userId")
+
+            supportActionBar?.subtitle = userName
+        }
     }
 }
