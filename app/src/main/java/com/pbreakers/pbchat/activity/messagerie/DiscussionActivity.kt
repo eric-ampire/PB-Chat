3d6831b50cd8e25e3c1d02053a147cdc3477edff
Eric Ampire <ericampire.top@gmail.com>
2018-11-08 09:53:09 +0200

    Module de recuperation des discussions

--- app/src/main/java/com/pbreakers/pbchat/activity/messagerie/DiscussionActivity.kt
+++ app/src/main/java/com/pbreakers/pbchat/activity/messagerie/DiscussionActivity.kt
@@ -3,11 +3,47 @@ package com.pbreakers.pbchat.activity.messagerie
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.pbreakers.pbchat.R
+import com.sendbird.android.GroupChannel
+import com.sendbird.android.GroupChannelListQuery
+import com.sendbird.android.SendBird
+import com.xwray.groupie.GroupAdapter
+import com.xwray.groupie.Item
+import com.xwray.groupie.ViewHolder
+import kotlinx.android.synthetic.main.activity_discussion.*
+import kotlinx.android.synthetic.main.item_contact.view.*
 
 class DiscussionActivity : AppCompatActivity() {
+
+    class DiscussionItem(val groupChannel: GroupChannel) : Item<ViewHolder>() {
+        override fun getLayout(): Int = R.layout.item_contact
+
+        override fun bind(viewHolder: ViewHolder, position: Int) {
+            with(viewHolder.itemView) {
+                userName.text = groupChannel.name
+                isOnLine.text = "${groupChannel.lastMessage} ${groupChannel.lastMessage.createdAt}"
+            }
+        }
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_discussion)
+        supportActionBar?.title = "Discussion 😃"
+
+        val adapter = GroupAdapter<ViewHolder>()
+        rvListDiscussion.adapter = adapter
+
+        val query  = GroupChannel.createMyGroupChannelListQuery()
+        query.next { mutableList, sendBirdException ->
+            if (sendBirdException != null) {
+                toast(sendBirdException.message)
+                return@next
+            }
+
+            mutableList.forEach {
+                adapter.add(DiscussionItem(it))
+                adapter.notifyDataSetChanged()
+            }
+        }
     }
 }
