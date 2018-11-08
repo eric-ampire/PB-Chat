9ebf64f3cf020ba9729cd5ccf16f14e1161e67f6
Eric Ampire <ericampire.top@gmail.com>
2018-11-08 08:25:41 +0200

    Mise en place de DiscussionActivity et son layout

---
+++ app/src/main/java/com/pbreakers/pbchat/activity/messagerie/DiscussionActivity.kt
@@ -0,0 +1,13 @@
+package com.pbreakers.pbchat.activity.messagerie
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.pbreakers.pbchat.R
+
+class DiscussionActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_discussion)
+    }
+}
