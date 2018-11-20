b46cfb6c096c2a3399abbdb563bf972a609e07aa
Eric Ampire <ericampire.top@gmail.com>
2018-11-20 15:40:17 +0200

    [UPDATE] Refactoring

--- app/src/main/java/com/pbreakers/pbchat/activity/login/LoginActivity.kt
+++ app/src/main/java/com/pbreakers/pbchat/activity/login/LoginActivity.kt
@@ -1,15 +1,23 @@
 package com.pbreakers.pbchat.activity.login
 
+import android.app.Activity
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.recyclerview.widget.LinearLayoutManager
+import com.firebase.ui.auth.AuthUI
+import com.firebase.ui.auth.IdpResponse
+import com.google.firebase.auth.FirebaseAuth
 import com.pbreakers.pbchat.R
 
 class LoginActivity : AppCompatActivity() {
 
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
 
     }
 }
