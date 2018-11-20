c82ceb5337ca889aa05da0f47d6132052e2f7b34
Eric Ampire <ericampire.top@gmail.com>
2018-11-20 15:40:43 +0200

    [UPDATE] Return to classic splash

--- app/src/main/java/com/pbreakers/pbchat/activity/login/SplashActivity.kt
+++ app/src/main/java/com/pbreakers/pbchat/activity/login/SplashActivity.kt
@@ -3,15 +3,9 @@ package com.pbreakers.pbchat.activity.login
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
-import android.view.View
-import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
 import com.pbreakers.pbchat.R
-import com.pbreakers.pbchat.activity.messagerie.ContactActivity
 import com.pbreakers.pbchat.activity.messagerie.DiscussionActivity
-import com.pbreakers.pbchat.util.SendBirdConfig
-import com.sendbird.android.*
-import kotlinx.android.synthetic.main.activity_splash.*
 
 
 class SplashActivity : AppCompatActivity() {
@@ -20,25 +14,19 @@ class SplashActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
 
-        SendBird.init(SendBirdConfig.applicationId, this)
+        object : Thread() {
+            override fun run() {
+                Thread.sleep(1000)
 
-        connect.setOnClickListener {
-            SendBird.connect("user2") { user, exception ->
-                if (exception != null) {
-                    Toast.makeText(baseContext, exception.message, Toast.LENGTH_LONG).show()
-                    Log.e("ericampire", exception.message)
+                if (FirebaseAuth.getInstance().currentUser == null) {
+                    startActivity(Intent(baseContext, LoginActivity::class.java))
+                    finish()
 
-                    return@connect
+                } else {
+                    startActivity(Intent(baseContext, DiscussionActivity::class.java))
+                    finish()
                 }
-
-                startActivity(Intent(this@SplashActivity, DiscussionActivity::class.java))
             }
-        }
-
-        disconnect.setOnClickListener {
-            SendBird.disconnect {
-                Toast.makeText(baseContext, "Vous est deconnecter", Toast.LENGTH_LONG).show()
-            }
-        }
+        }.start()
     }
 }
