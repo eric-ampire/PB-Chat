d83bf5f95d2417771fee34aa9e706c62904793b5
Eric Ampire <ericampire.top@gmail.com>
2018-11-20 15:49:55 +0200

    [UPDATE] Refactoring

--- app/src/main/java/com/pbreakers/pbchat/activity/messagerie/DetailDiscussionActivity.kt
+++ app/src/main/java/com/pbreakers/pbchat/activity/messagerie/DetailDiscussionActivity.kt
@@ -12,8 +12,6 @@ import kotlinx.android.synthetic.main.item_contact.view.*
 
 class DetailDiscussionActivity : AppCompatActivity() {
 
-    private var channel: GroupChannel? = null
-
     class MessageItem(val message: UserMessage) : Item<ViewHolder>() {
         override fun getLayout(): Int = R.layout.item_contact
 
@@ -28,68 +26,5 @@ class DetailDiscussionActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_detail_discussion)
-
-        val adapter = GroupAdapter<ViewHolder>()
-        rvListMessage.adapter = adapter
-
-        var userId: String
-        var userProfile: String
-
-        with(intent) {
-
-            val userName = getStringExtra("userName")
-            userProfile = getStringExtra("userProfile")
-            userId = getStringExtra("userId")
-
-            supportActionBar?.subtitle = userName
-        }
-
-
-        sendButton.setOnClickListener {
-            val messageText = tapedMessage.text.toString()
-
-            val ids = mutableListOf<String>(userId, SendBird.getCurrentUser().userId)
-            tapedMessage.text.clear()
-
-
-            val params = GroupChannelParams()
-                .setName(userProfile)
-                .setDistinct(true)
-                .setPublic(false)
-                .addUserIds(ids)
-
-            GroupChannel.createChannel(params) { e, a ->
-                if (a != null) {
-                    toast(a.message)
-                    return@createChannel
-                }
-
-                channel = e
-
-                e.sendUserMessage(messageText) { a, error ->
-                    if (error != null) {
-                        toast(error.message)
-                        return@sendUserMessage
-                    }
-
-                    val previewMessage = channel!!.createPreviousMessageListQuery()
-                    previewMessage.load { e, err ->
-                        if (err != null) {
-                            toast(err.message)
-                            return@load
-                        }
-
-                        e.forEach {
-                            adapter.add(MessageItem(it as UserMessage))
-                            adapter.notifyDataSetChanged()
-                        }
-                    }
-
-                    toast("Message non lu " + e.unreadMessageCount)
-
-                    toast("message send")
-                }
-            }
-        }
     }
 }
