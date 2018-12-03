ac891adc03bd83c2e7f7681a6d6b4aa5d5ed91e3
Eric Ampire <ericampire.top@gmail.com>
2018-12-03 20:58:44 +0200

    Connexion du menu au fragment

--- app/src/main/java/com/pbreakers/pbchat/fragment/home/ProfileFragment.kt
+++ app/src/main/java/com/pbreakers/pbchat/fragment/home/ProfileFragment.kt
@@ -2,10 +2,9 @@ package com.pbreakers.pbchat.fragment.home
 
 
 import android.os.Bundle
+import android.view.*
+import androidx.appcompat.app.AlertDialog
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
 import androidx.appcompat.app.AppCompatActivity
 
 import com.pbreakers.pbchat.R
@@ -29,6 +28,42 @@ class ProfileFragment : Fragment() {
         val appCompatActivity = activity as AppCompatActivity
 
         appCompatActivity.supportActionBar?.title = "Profile"
+        setHasOptionsMenu(true)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when(item.itemId) {
+            R.id.navigation_edit -> {
+                true
+            }
+
+            R.id.navigation_logout -> {
+                AlertDialog.Builder(activity as AppCompatActivity)
+                    .setTitle("Deconnexion")
+                    .setMessage("Etes vous sur de vouloir quitter ?")
+                    .setNegativeButton("Non") { dialog, b ->
+                        dialog.dismiss()
+                    }.setPositiveButton("Oui") { _, _ ->
+                        tryToLogout()
+                    }.show()
+
+
+                true
+            }
+
+            else -> {
+                super.onOptionsItemSelected(item)
+            }
+        }
+    }
+
+    private fun tryToLogout() {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?, inflater: MenuInflater) {
+        super.onCreateOptionsMenu(menu, inflater)
+        inflater.inflate(R.menu.profile_menu, menu)
     }
 
 }
