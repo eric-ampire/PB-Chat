079bc625f058f6d406c1e7f65e426925912aadb9
Eric Ampire <ericampire.top@gmail.com>
2018-12-03 08:20:11 +0200

    Ajout de l'application [Instance de la classe Application]

---
+++ app/src/main/java/com/pbreakers/pbchat/application/PBChatApplication.kt
@@ -0,0 +1,11 @@
+package com.pbreakers.pbchat.application
+
+import android.app.Application
+
+class PBChatApplication : Application() {
+    override fun onCreate() {
+        super.onCreate()
+
+
+    }
+}
