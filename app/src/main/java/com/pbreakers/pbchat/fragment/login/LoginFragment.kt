211f4ba7a514698ee6b0b2f5236eba49fbd41d44
Eric Ampire <ericampire.top@gmail.com>
2018-12-03 14:01:01 +0200

    Refactoring

--- app/src/main/java/com/pbreakers/pbchat/fragment/login/LoginFragment.kt
+++ app/src/main/java/com/pbreakers/pbchat/fragment/login/LoginFragment.kt
@@ -1,35 +1,32 @@
-package com.pbreakers.pbchat.fragment
+package com.pbreakers.pbchat.fragment.login
 
 
 import android.app.ProgressDialog
-import android.content.Intent
 import android.os.Bundle
 import android.view.*
 import android.widget.Toast
 import androidx.fragment.app.Fragment
-
 import com.pbreakers.pbchat.R
-import com.pbreakers.pbchat.activity.MainActivity
 import com.quickblox.auth.QBAuth
 import com.quickblox.auth.session.QBSession
 import com.quickblox.core.QBEntityCallback
 import com.quickblox.core.exception.QBResponseException
-import com.quickblox.users.QBUsers
 import com.quickblox.users.model.QBUser
-import kotlinx.android.synthetic.main.fragment_registration.*
+import kotlinx.android.synthetic.main.fragment_login.*
 
-class RegistrationFragment : Fragment() {
+
+class LoginFragment : Fragment() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        activity?.title = "Login"
+        setHasOptionsMenu(true)
+    }
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_registration, container, false)
-    }
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        activity?.title = "Compte"
-        setHasOptionsMenu(true)
+        return inflater.inflate(R.layout.fragment_login, container, false)
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
@@ -44,7 +41,7 @@ class RegistrationFragment : Fragment() {
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         return when(item.itemId) {
             R.id.menu_login -> {
-                tryToSignUp()
+                tryToSignIn()
                 true
             } else -> {
                 return super.onOptionsItemSelected(item)
@@ -52,26 +49,21 @@ class RegistrationFragment : Fragment() {
         }
     }
 
-    private fun tryToSignUp() {
+    private fun tryToSignIn() {
         val username = edLogin.text.toString()
         val password = edPassword.text.toString()
-        val fullName = edPseudo.text.toString()
 
         val dialog = ProgressDialog(activity).apply {
-            setTitle("Creation en cour")
+            setTitle("Connexion")
             setMessage("Veuillez patientez s'il vous plait !")
         }
 
         dialog.show()
 
-        val user = QBUser(username, password).apply {
-            this.fullName = fullName
-        }
-
-        QBUsers.signUp(user).performAsync(object : QBEntityCallback<QBUser> {
-            override fun onSuccess(user: QBUser, bundle: Bundle) {
+        QBAuth.createSession(QBUser(username, password)).performAsync(object : QBEntityCallback<QBSession> {
+            override fun onSuccess(session: QBSession, bundle: Bundle) {
                 dialog.dismiss()
-                startActivity(Intent(activity, MainActivity::class.java))
+                //startActivity(Intent(activity, MainActivity::class.java))
                 activity?.finish()
             }
 
--- app/src/main/java/com/pbreakers/pbchat/fragment/login/RegistrationFragment.kt
+++ app/src/main/java/com/pbreakers/pbchat/fragment/login/RegistrationFragment.kt
@@ -1,37 +1,31 @@
-package com.pbreakers.pbchat.fragment
+package com.pbreakers.pbchat.fragment.login
 
 
 import android.app.ProgressDialog
-import android.content.Intent
 import android.os.Bundle
-import android.util.Log
 import android.view.*
 import android.widget.Toast
 import androidx.fragment.app.Fragment
+
 import com.pbreakers.pbchat.R
-import com.pbreakers.pbchat.activity.MainActivity
-import com.pbreakers.pbchat.util.QBUtil
-import com.quickblox.auth.QBAuth
-import com.quickblox.auth.session.QBSession
 import com.quickblox.core.QBEntityCallback
 import com.quickblox.core.exception.QBResponseException
+import com.quickblox.users.QBUsers
 import com.quickblox.users.model.QBUser
-import kotlinx.android.synthetic.*
-import kotlinx.android.synthetic.main.fragment_login.*
+import kotlinx.android.synthetic.main.fragment_registration.*
 
-
-class LoginFragment : Fragment() {
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        activity?.title = "Login"
-        setHasOptionsMenu(true)
-    }
+class RegistrationFragment : Fragment() {
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_login, container, false)
+        return inflater.inflate(R.layout.fragment_registration, container, false)
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        activity?.title = "Compte"
+        setHasOptionsMenu(true)
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
@@ -46,7 +40,7 @@ class LoginFragment : Fragment() {
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         return when(item.itemId) {
             R.id.menu_login -> {
-                tryToSignIn()
+                tryToSignUp()
                 true
             } else -> {
                 return super.onOptionsItemSelected(item)
@@ -54,22 +48,27 @@ class LoginFragment : Fragment() {
         }
     }
 
-    private fun tryToSignIn() {
+    private fun tryToSignUp() {
         val username = edLogin.text.toString()
         val password = edPassword.text.toString()
+        val fullName = edPseudo.text.toString()
 
         val dialog = ProgressDialog(activity).apply {
-            setTitle("Connexion")
+            setTitle("Creation en cour")
             setMessage("Veuillez patientez s'il vous plait !")
         }
 
         dialog.show()
 
-        QBAuth.createSession(QBUser(username, password)).performAsync(object : QBEntityCallback<QBSession> {
-            override fun onSuccess(session: QBSession, bundle: Bundle) {
+        val user = QBUser(username, password).apply {
+            this.fullName = fullName
+        }
+
+        QBUsers.signUp(user).performAsync(object : QBEntityCallback<QBUser> {
+            override fun onSuccess(user: QBUser, bundle: Bundle) {
                 dialog.dismiss()
-                startActivity(Intent(activity, MainActivity::class.java))
-                activity?.finish()
+                Toast.makeText(activity, "Votre compte a etait crée avec succes !", Toast.LENGTH_LONG).show()
+
             }
 
             override fun onError(error: QBResponseException) {
