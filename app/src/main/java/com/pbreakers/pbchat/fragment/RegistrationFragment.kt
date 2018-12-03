0bf820ef9976224168fdc9be1a90215860cf9866
Eric Ampire <ericampire.top@gmail.com>
2018-12-03 12:16:32 +0200

    Ajout d'un menu au fragment

--- app/src/main/java/com/pbreakers/pbchat/fragment/RegistrationFragment.kt
+++ app/src/main/java/com/pbreakers/pbchat/fragment/RegistrationFragment.kt
@@ -2,10 +2,8 @@ package com.pbreakers.pbchat.fragment
 
 
 import android.os.Bundle
+import android.view.*
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
 
 import com.pbreakers.pbchat.R
 
@@ -15,6 +13,31 @@ class RegistrationFragment : Fragment() {
                               savedInstanceState: Bundle?): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_registration, container, false)
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        activity?.title = "Compte"
+        setHasOptionsMenu(true)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        super.onCreateOptionsMenu(menu, inflater)
+        inflater.inflate(R.menu.login, menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when(item.itemId) {
+            R.id.menu_login -> {
+                true
+            } else -> {
+                return super.onOptionsItemSelected(item)
+            }
+        }
     }
 
 
