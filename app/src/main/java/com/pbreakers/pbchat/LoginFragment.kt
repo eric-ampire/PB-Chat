c0814061371e70c735b52cba4950690f5c4aa67d
Eric Ampire <ericampire.top@gmail.com>
2018-12-03 12:12:46 +0200

    Ajout du fragment login

---
+++ app/src/main/java/com/pbreakers/pbchat/LoginFragment.kt
@@ -0,0 +1,41 @@
+package com.pbreakers.pbchat
+
+
+import android.os.Bundle
+import android.view.*
+import androidx.fragment.app.Fragment
+
+
+class LoginFragment : Fragment() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        activity?.title = "Login"
+        setHasOptionsMenu(true)
+    }
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_login, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        super.onCreateOptionsMenu(menu, inflater)
+        inflater.inflate(R.menu.login, menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when(item.itemId) {
+            R.id.menu_login -> {
+                true
+            } else -> {
+                return super.onOptionsItemSelected(item)
+            }
+        }
+    }
+}
