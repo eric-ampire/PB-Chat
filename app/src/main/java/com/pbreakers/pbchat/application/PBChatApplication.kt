5ce5c73fdd1afceb05af02d02d937bb5a36a49de
Eric Ampire <ericampire.top@gmail.com>
2018-12-03 13:45:32 +0200

    Mise a jour account key

--- app/src/main/java/com/pbreakers/pbchat/application/PBChatApplication.kt
+++ app/src/main/java/com/pbreakers/pbchat/application/PBChatApplication.kt
@@ -6,7 +6,7 @@ import com.quickblox.auth.session.QBSettings
 class PBChatApplication : Application() {
 
     companion object {
-        const val ACCOUNT_KEY = "008577"
+        const val ACCOUNT_KEY = "4sZ3iiAwSXd2vpvGvz4-"
         const val APP_ID = "74066"
         const val AUTHORIZATION_KEY = "buGva2DAcGLDmjK"
         const val AUTHORIZATION_SECRET = "3GUMWaZmK3qKp7L"
