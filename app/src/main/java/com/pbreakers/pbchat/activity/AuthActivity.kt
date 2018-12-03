566cb2541d9790d1c01c9e9506259be4a88bfaeb
Eric Ampire <ericampire.top@gmail.com>
2018-12-03 12:28:53 +0200

    Definition du fragment par defaut

--- app/src/main/java/com/pbreakers/pbchat/activity/AuthActivity.kt
+++ app/src/main/java/com/pbreakers/pbchat/activity/AuthActivity.kt
@@ -1,34 +1,43 @@
 package com.pbreakers.pbchat.activity
 
 import android.os.Bundle
+import android.view.MenuItem
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import com.pbreakers.pbchat.R
+import com.pbreakers.pbchat.fragment.LoginFragment
+import com.pbreakers.pbchat.fragment.RegistrationFragment
 import kotlinx.android.synthetic.main.activity_auth.*
 
-class AuthActivity : AppCompatActivity() {
-
-    private val mOnNavigationItemSelectedListener = BottomNavigationView.OnNavigationItemSelectedListener { item ->
-        when (item.itemId) {
-            R.id.navigation_home -> {
-                message.setText(R.string.title_home)
-                return@OnNavigationItemSelectedListener true
+class AuthActivity : AppCompatActivity(), BottomNavigationView.OnNavigationItemSelectedListener {
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            R.id.navigation_login -> {
+                replaceFragment(LoginFragment())
+                true
             }
-            R.id.navigation_dashboard -> {
-                message.setText(R.string.title_dashboard)
-                return@OnNavigationItemSelectedListener true
+            R.id.navigation_create_account -> {
+                replaceFragment(RegistrationFragment())
+                true
             }
-            R.id.navigation_notifications -> {
-                message.setText(R.string.title_notifications)
-                return@OnNavigationItemSelectedListener true
+            else -> {
+                false
             }
         }
-        false
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_auth)
 
-        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener)
+        navigation.setOnNavigationItemSelectedListener(this)
+        replaceFragment(LoginFragment())
+    }
+
+    private fun replaceFragment(fragment: Fragment) {
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.frameLayout, fragment)
+            .commit()
     }
 }
