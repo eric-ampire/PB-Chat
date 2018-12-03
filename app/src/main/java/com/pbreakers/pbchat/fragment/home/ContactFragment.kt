9339d5eb5caabd5f9eb85d23129d5c6ae65bc760
Eric Ampire <ericampire.top@gmail.com>
2018-12-03 14:04:40 +0200

    Ajout du fragment qui va afficher la listes de contact

---
+++ app/src/main/java/com/pbreakers/pbchat/fragment/home/ContactFragment.kt
@@ -0,0 +1,29 @@
+package com.pbreakers.pbchat.fragment.home
+
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+import com.pbreakers.pbchat.R
+
+
+class ContactFragment : Fragment() {
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_contact, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        activity?.title = "Discussion"
+    }
+}
