61a3d95faa68ab2d3a3afb987dc69a27ceb7857e
Eric Ampire <ericampire.top@gmail.com>
2018-12-03 14:21:39 +0200

    Ajout du main activity

---
+++ app/src/main/java/com/pbreakers/pbchat/activity/MainActivity.kt
@@ -0,0 +1,44 @@
+package com.pbreakers.pbchat.activity
+
+import android.os.Bundle
+import android.view.MenuItem
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import androidx.appcompat.app.AppCompatActivity
+import com.pbreakers.pbchat.R
+import com.pbreakers.pbchat.fragment.home.ContactFragment
+import com.pbreakers.pbchat.fragment.home.DiscussionFragment
+import com.pbreakers.pbchat.fragment.home.ProfileFragment
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity(), BottomNavigationView.OnNavigationItemSelectedListener {
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+        return when(item.itemId) {
+            R.id.navigation_dialog -> {
+                AuthActivity.replaceFragment(supportFragmentManager, DiscussionFragment())
+                true
+            }
+
+            R.id.navigation_profile -> {
+                AuthActivity.replaceFragment(supportFragmentManager, ProfileFragment())
+                true
+            }
+
+            R.id.navigation_contact -> {
+                AuthActivity.replaceFragment(supportFragmentManager, ContactFragment())
+                true
+            }
+
+            else -> {
+                false
+            }
+        }
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        homeNavigation.setOnNavigationItemSelectedListener(this)
+        AuthActivity.replaceFragment(supportFragmentManager, DiscussionFragment())
+    }
+}
