35841fb57970acf2bb45b7596134c7fdf723d62c
Eric Ampire <ericampire.top@gmail.com>
2018-12-03 10:47:09 +0200

    Mise en place de tout le necessaire pour le login

---
+++ app/src/main/java/com/pbreakers/pbchat/activity/AuthActivity.kt
@@ -0,0 +1,34 @@
+package com.pbreakers.pbchat.activity
+
+import android.os.Bundle
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_auth.*
+
+class AuthActivity : AppCompatActivity() {
+
+    private val mOnNavigationItemSelectedListener = BottomNavigationView.OnNavigationItemSelectedListener { item ->
+        when (item.itemId) {
+            R.id.navigation_home -> {
+                message.setText(R.string.title_home)
+                return@OnNavigationItemSelectedListener true
+            }
+            R.id.navigation_dashboard -> {
+                message.setText(R.string.title_dashboard)
+                return@OnNavigationItemSelectedListener true
+            }
+            R.id.navigation_notifications -> {
+                message.setText(R.string.title_notifications)
+                return@OnNavigationItemSelectedListener true
+            }
+        }
+        false
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_auth)
+
+        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener)
+    }
+}
--- app/src/main/java/com/pbreakers/pbchat/application/PBChatApplication.kt
+++ app/src/main/java/com/pbreakers/pbchat/application/PBChatApplication.kt
@@ -1,11 +1,21 @@
 package com.pbreakers.pbchat.application
 
 import android.app.Application
+import com.quickblox.auth.session.QBSettings
 
 class PBChatApplication : Application() {
+
+    companion object {
+        const val ACCOUNT_KEY = "008577"
+        const val APP_ID = "74066"
+        const val AUTHORIZATION_KEY = "buGva2DAcGLDmjK"
+        const val AUTHORIZATION_SECRET = "3GUMWaZmK3qKp7L"
+    }
+
     override fun onCreate() {
         super.onCreate()
 
-
+        QBSettings.getInstance().init(applicationContext, APP_ID, AUTHORIZATION_KEY, AUTHORIZATION_SECRET)
+        QBSettings.getInstance().accountKey = ACCOUNT_KEY
     }
 }
