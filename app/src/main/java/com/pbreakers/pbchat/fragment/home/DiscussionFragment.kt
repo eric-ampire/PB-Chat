49f3bf55ffe45027370e59989806837b7dc37ceb
Eric Ampire <ericampire.top@gmail.com>
2018-12-04 15:35:40 +0200

    Adding listener to swipeRefreshLayout

--- app/src/main/java/com/pbreakers/pbchat/fragment/home/DiscussionFragment.kt
+++ app/src/main/java/com/pbreakers/pbchat/fragment/home/DiscussionFragment.kt
@@ -6,8 +6,13 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.SnapHelper
+import com.google.android.material.snackbar.Snackbar
 
 import com.pbreakers.pbchat.R
 import com.quickblox.chat.QBRestChatService
@@ -16,17 +21,20 @@ import com.quickblox.core.QBEntityCallback
 import com.quickblox.core.exception.QBResponseException
 import com.quickblox.core.request.QBRequestBuilder
 import com.quickblox.core.request.QBRequestGetBuilder
-import java.util.ArrayList
+import com.squareup.picasso.Picasso
+import com.stfalcon.chatkit.commons.ImageLoader
+import com.stfalcon.chatkit.commons.models.IDialog
+import com.stfalcon.chatkit.commons.models.IMessage
+import com.stfalcon.chatkit.dialogs.DialogsListAdapter
+import com.xwray.groupie.GroupAdapter
+import com.xwray.groupie.Item
+import com.xwray.groupie.ViewHolder
+import kotlinx.android.synthetic.main.fragment_discussion.*
+import kotlinx.android.synthetic.main.fragment_discussion.view.*
+import java.util.*
 
 
-class DiscussionFragment : Fragment(), QBEntityCallback<ArrayList<QBChatDialog>> {
-    override fun onSuccess(dialogs: ArrayList<QBChatDialog>, bundle: Bundle) {
-        Toast.makeText(activity, dialogs.first().name, Toast.LENGTH_LONG).show()
-    }
-
-    override fun onError(error: QBResponseException) {
-        Toast.makeText(activity, error.message, Toast.LENGTH_LONG).show()
-    }
+class DiscussionFragment : Fragment() {
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
@@ -36,6 +44,13 @@ class DiscussionFragment : Fragment(), QBEntityCallback<ArrayList<QBChatDialog>>
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        dialogRefreshing.isRefreshing = true
+        dialogRefreshing.setOnRefreshListener {
+            getAllDialogs(view)
+        }
+    }
+
+    private fun getAllDialogs(view: View) {
         val requestBuilder = QBRequestGetBuilder().apply {
             limit = 100
         }
@@ -48,5 +63,19 @@ class DiscussionFragment : Fragment(), QBEntityCallback<ArrayList<QBChatDialog>>
         val appCompatActivity = activity as AppCompatActivity
 
         appCompatActivity.supportActionBar?.title = "Discussion"
+    }
+
+    class DialogItem(val qbDialog: QBChatDialog) : Item<ViewHolder>() {
+        override fun getLayout(): Int = R.layout.item_dialog
+
+        override fun bind(viewHolder: ViewHolder, position: Int) {
+            with(viewHolder.itemView) {
+                findViewById<TextView>(R.id.dialogName).text = qbDialog.name
+                findViewById<TextView>(R.id.dialogDate).text = "Date"
+                findViewById<TextView>(R.id.dialogLastMessage).text = qbDialog.lastMessage
+                val avatar = findViewById<ImageView>(R.id.dialogAvatar)
+                Picasso.get().load(qbDialog.photo).error(R.drawable.logo).into(avatar)
+            }
+        }
     }
 }
