07778e1cdbb274e7628c12df6b0327cf298ed8ed
Eric Ampire <ericampire.top@gmail.com>
2018-12-04 15:36:39 +0200

    Adding listener to swipeRefreshLayout

--- app/src/main/java/com/pbreakers/pbchat/fragment/home/ContactFragment.kt
+++ app/src/main/java/com/pbreakers/pbchat/fragment/home/ContactFragment.kt
@@ -6,9 +6,18 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 
 import com.pbreakers.pbchat.R
+import com.quickblox.core.QBEntityCallback
+import com.quickblox.core.exception.QBResponseException
+import com.quickblox.core.request.QBPagedRequestBuilder
+import com.quickblox.core.request.QBRequestGetBuilder
+import com.quickblox.users.QBUsers
+import com.quickblox.users.model.QBUser
+import kotlinx.android.synthetic.main.fragment_contact.*
+import kotlinx.android.synthetic.main.fragment_contact.view.*
 
 
 class ContactFragment : Fragment() {
@@ -21,6 +30,15 @@ class ContactFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        contactRefreshing.isRefreshing = true
+        contactRefreshing.setOnRefreshListener {
+            getAllContact()
+        }
+    }
+
+    private fun getAllContact() {
+
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
