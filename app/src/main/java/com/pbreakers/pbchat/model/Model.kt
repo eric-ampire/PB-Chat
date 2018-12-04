126f0b53d6a157b4bb9f8be87fadb8ada235c6d0
Eric Ampire <ericampire.top@gmail.com>
2018-12-04 22:36:15 +0200

    Refactoring du model

--- app/src/main/java/com/pbreakers/pbchat/model/Model.kt
+++ app/src/main/java/com/pbreakers/pbchat/model/Model.kt
@@ -2,6 +2,7 @@ package com.pbreakers.pbchat.model
 
 import com.quickblox.chat.model.QBChatDialog
 import com.quickblox.chat.model.QBChatMessage
+import com.quickblox.users.QBUsers
 import com.quickblox.users.model.QBUser
 import com.stfalcon.chatkit.commons.models.IDialog
 import com.stfalcon.chatkit.commons.models.IMessage
@@ -18,11 +19,19 @@ class PBMessage(val qbMessage: QBChatMessage) : IMessage {
     override fun getId(): String = qbMessage.id
     override fun getCreatedAt(): Date = Date(qbMessage.dateSent)
     override fun getUser(): IUser {
-        TODO()
+        val qbUser = QBUsers.getUser(qbMessage.senderId).perform()
+        return qbUser.toIUser()
     }
 
-
     override fun getText(): String = qbMessage.body
+}
+
+fun QBUser.toIUser(): IUser {
+    return PBUser(this)
+}
+
+fun QBChatMessage.toIMessage(): IMessage {
+    return PBMessage(this)
 }
 
 class PBDialog(val qbChatDialog: QBChatDialog) : IDialog<IMessage> {
@@ -32,11 +41,28 @@ class PBDialog(val qbChatDialog: QBChatDialog) : IDialog<IMessage> {
 
     override fun getId(): String = qbChatDialog.id.toString()
     override fun getUsers(): MutableList<out IUser> {
-        TODO()
+        val users = arrayListOf<IUser>()
+        qbChatDialog.occupants.asSequence().forEach {
+            val user = QBUsers.getUser(it).perform()
+            users += user.toIUser()
+        }
+
+        return users
     }
 
     override fun getLastMessage(): IMessage {
-        TODO()
+
+        return object : IMessage {
+            override fun getId(): String = UUID.randomUUID().toString()
+            override fun getCreatedAt(): Date = Date(qbChatDialog.lastMessageDateSent)
+
+            override fun getUser(): IUser {
+                val user = QBUsers.getUser(qbChatDialog.lastMessageUserId).perform()
+                return user.toIUser()
+            }
+
+            override fun getText(): String = qbChatDialog.lastMessage
+        }
     }
 
     override fun getDialogName(): String = qbChatDialog.name
