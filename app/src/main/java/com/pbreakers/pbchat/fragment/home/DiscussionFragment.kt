1b54d1263a6e5eca35ac1944505f98ac85ad1bd4
Eric Ampire <ericampire.top@gmail.com>
2018-12-04 12:16:26 +0200

    Recuperation des dialog

--- app/src/main/java/com/pbreakers/pbchat/fragment/home/DiscussionFragment.kt
+++ app/src/main/java/com/pbreakers/pbchat/fragment/home/DiscussionFragment.kt
@@ -6,12 +6,27 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 
 import com.pbreakers.pbchat.R
+import com.quickblox.chat.QBRestChatService
+import com.quickblox.chat.model.QBChatDialog
+import com.quickblox.core.QBEntityCallback
+import com.quickblox.core.exception.QBResponseException
+import com.quickblox.core.request.QBRequestBuilder
+import com.quickblox.core.request.QBRequestGetBuilder
+import java.util.ArrayList
 
 
-class DiscussionFragment : Fragment() {
+class DiscussionFragment : Fragment(), QBEntityCallback<ArrayList<QBChatDialog>> {
+    override fun onSuccess(dialogs: ArrayList<QBChatDialog>, bundle: Bundle) {
+        Toast.makeText(activity, dialogs.first().name, Toast.LENGTH_LONG).show()
+    }
+
+    override fun onError(error: QBResponseException) {
+        Toast.makeText(activity, error.message, Toast.LENGTH_LONG).show()
+    }
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
@@ -20,6 +35,12 @@ class DiscussionFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        val requestBuilder = QBRequestGetBuilder().apply {
+            limit = 100
+        }
+
+        QBRestChatService.getChatDialogs(null, requestBuilder).performAsync(this)
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
