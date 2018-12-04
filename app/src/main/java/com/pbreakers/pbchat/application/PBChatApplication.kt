8ac109b2d98fb2b7c892f99eebd0f23f3d9c05dd
Eric Ampire <ericampire.top@gmail.com>
2018-12-04 22:35:54 +0200

    Configuration de l'application

--- app/src/main/java/com/pbreakers/pbchat/application/PBChatApplication.kt
+++ app/src/main/java/com/pbreakers/pbchat/application/PBChatApplication.kt
@@ -2,6 +2,9 @@ package com.pbreakers.pbchat.application
 
 import android.app.Application
 import com.quickblox.auth.session.QBSettings
+import com.quickblox.chat.QBChatService
+import com.quickblox.core.QBHttpConnectionConfig
+import com.quickblox.core.StoringMechanism
 
 class PBChatApplication : Application() {
 
@@ -15,7 +18,18 @@ class PBChatApplication : Application() {
     override fun onCreate() {
         super.onCreate()
 
+        QBSettings.getInstance().storingMehanism = StoringMechanism.UNSECURED
         QBSettings.getInstance().init(applicationContext, APP_ID, AUTHORIZATION_KEY, AUTHORIZATION_SECRET)
         QBSettings.getInstance().accountKey = ACCOUNT_KEY
+        QBHttpConnectionConfig.setConnectTimeout(9000)
+
+        QBChatService.setDefaultPacketReplyTimeout(9000)
+
+        val chatServiceConfigurationBuilder = QBChatService.ConfigurationBuilder()
+        chatServiceConfigurationBuilder.socketTimeout = 60 //Sets chat socket's read timeout in seconds
+        chatServiceConfigurationBuilder.isKeepAlive = true //Sets connection socket's keepAlive option.
+        chatServiceConfigurationBuilder.isUseTls = true //Sets the TLS security mode used when making the connection. By default TLS is disabled.
+        QBChatService.setConfigurationBuilder(chatServiceConfigurationBuilder)
+
     }
 }
